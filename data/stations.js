152dcb582a310b7b50bfd91519c127d960f35ba0
Matt Sherman <mwsherman@gmail.com>
2013-03-02 16:57:51 -0500

    add mediaelement.js and players on station

--- data/stations.js
+++ data/stations.js
@@ -2,12 +2,12 @@ var all = {
 	kdfc : {
 		name: 'KDFC',
 		location: 'San Francisco',
-		stream_url: ''
+		stream_url: 'http://9313.live.streamtheworld.com:80/KDFCFM_SC'
 	},
 	wqxr : {
 		name: 'WQXR',
 		location: 'New York',
-		stream_url: ''
+		stream_url: 'http://fm939.wnyc.org/'
 	}
 }
 
