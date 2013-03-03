d8f204996c6e590aeba61a8a717a942494ff337f
Matt Sherman <mwsherman@gmail.com>
2013-03-03 17:13:06 -0500

    only use fastClick on mobile

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -1,6 +1,7 @@
 $(function(){
 	var pop = $("span.pop");
-	pop.fastClick(function() {
+
+	pop.click(function() {
 		if (window['player'] != undefined) {
 			window.player.pause();
 		}
@@ -11,7 +12,10 @@ $(function(){
 		return false;
 	});
 
-	$("a").fastClick(function () {
-		top.window.location.href=$(this).attr('href');
-	});
+	var mobile = $("body").hasClass("mobile");
+	if (mobile) {
+		$("a").fastClick(function () {
+			top.window.location.href=$(this).attr('href');
+		});
+	}
 });
