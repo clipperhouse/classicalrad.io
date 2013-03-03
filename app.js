5d525d7a49ff2409cba166e13eda20ac0a6f60b2
Matt Sherman <mwsherman@gmail.com>
2013-03-03 01:28:37 -0500

    remove less-middleware

--- app.js
+++ app.js
@@ -17,7 +17,6 @@ app.configure(function(){
   app.use(express.cookieParser('your secret here'));
   app.use(express.session());
   app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
   app.use(express.static(path.join(__dirname, 'public')));
 });
 
