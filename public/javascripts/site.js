144b83b392945ec17f89e46b2f1281db07cedcc6
Matt Sherman <mwsherman@gmail.com>
2013-03-05 23:35:06 -0500

    popup can be a little smaller

--- public/javascripts/site.js
+++ public/javascripts/site.js
@@ -7,7 +7,7 @@ $(function(){
 		}
 		var url = $(this).data('url');
 		var width = 350;
-		window.open(url, 'pop', 'left=' + (screen.width - width) + ', width=' + width + ', height=400, location=no');
+		window.open(url, 'pop', 'left=' + (screen.width - width) + ', width=' + width + ', height=360, location=no');
 		document.location.href = '/';
 		return false;
 	});
