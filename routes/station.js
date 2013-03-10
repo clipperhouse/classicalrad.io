2c07d7c6b39502355f64b325ccf91794989c8b29
Matt Sherman <mwsherman@gmail.com>
2013-03-09 23:47:55 -0500

    add pls support to station route

--- routes/station.js
+++ routes/station.js
@@ -1,14 +1,35 @@
-var stations = require('../lib/stations');
-var utils = require('../lib/utils');
+var stations = require('../lib/stations')
+  , utils = require('../lib/utils')
+  , pls = require('../lib/pls-parser');
 
-exports.detail = function(req, res){
+exports.detail = function(req, res) {
 	var id = req.params.id;
 	var station = stations.byId(req, id);
-	var locals = {
-		title: station.name + ' | classicalrad.io',
-		bodyclass: utils.bodyClass(req, 'station'),
-		pop_url: req.path + '?pop',
-		station: station
+
+	var render = function(playlist) {
+		var stream_url = station.stream_url;
+		if (playlist) {
+			// get first item with 'file' property
+			var items = playlist.filter(function(item) {
+				return item.file;
+			});
+			if (items.length) {
+				stream_url = items[0].file;
+			}
+		}
+		var locals = {
+			title: station.name + ' | classicalrad.io',
+			bodyclass: utils.bodyClass(req, 'station'),
+			pop_url: req.path + '?pop',
+			station: station,
+			stream_url: stream_url
+		};
+		res.render('station', locals);
 	};
-	res.render('station', locals);
+
+	if (station.playlist_url) {
+		pls.fetch(station.playlist_url, render);
+	} else {
+		render();
+	}
 };
